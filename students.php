8a8044a798854409fdf09e079018b09e3a680968
Krikod <Krikod@users.noreply.github.com>
2017-03-13 23:46:08 +0100

    Update students.php

--- students.php
+++ students.php
@@ -5,10 +5,13 @@
  * Date: 13/03/17
  * Time: 17:33
  */
- $students = ["Emmanuel" => 42, "Thierry" => 51, "Pascal" => 45, "Eric" => 48, "Nicolas" => 19];
- foreach ($students as $name => $age) {
-     $average_age = array_sum($age) / count($age);
-     echo '<p>Les prénoms et l\'âge des élèves (liste)</p>' .
-         $name . ': ' . $age . '<br>' .
-         'La moyenne de l\'âge des élèves est' . $average_age . 'ans.';
- }
+$students = ["Emmanuel" => 42, "Thierry" => 51, "Pascal" => 45, "Eric" => 48, "Nicolas" => 19];
+
+print '<p>Les prénoms et l\'âge des élèves (liste):</p>';
+
+foreach ($students as $name => $age) {
+    echo $name . ': ' . $age . '<br>';
+}
+
+$average_age = array_sum($students) / count($students);
+echo '<p>La moyenne de l\'âge des élèves est ' . $average_age . ' ans.</p>';
